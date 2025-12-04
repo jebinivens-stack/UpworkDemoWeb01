338e810433318bcb417b16136a3a51f82d13964e
jebinivens-stack <jebinivens@gmail.com>
2025-12-04 11:55:11 -0500

    Update UpworkDemo.php

--- UpworkDemo.php
+++ UpworkDemo.php
@@ -1,2 +1,2 @@
-<?php echo "Upwork Demo File";
-echo "This is my test website on Azure.  I deployed it via an Azure Web App";
+<?php echo "Upwork Demo File. This is my test website on Azure.  I deployed it via an Azure Web App";
+
